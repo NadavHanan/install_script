b91e9d7e1419ab15ec8cd3540e5234ef5d15b86e
Nadav Hanan <nadavh12@gmail.com>
2026-09-14 23:34:44 +0300

    add screanshot and improved pkgs install, remove, and update scripts

--- dotfiles/hypr/bindings.lua
+++ dotfiles/hypr/bindings.lua
@@ -25,7 +25,7 @@ hl.bind(mainMod .. " + CTRL + B", launch_tui("bluetui"))
 hl.bind(mainMod .. " + CTRL + A", launch_tui("wiremix"))
 hl.bind(mainMod .. " + C", launch_tui("qalc", { 480, 300 }))
 
-hl.bind("Print", hl.dsp.exec_cmd('grim -g "$(slurp)" - | wl-copy'))
+hl.bind("Print", hl.dsp.exec_cmd("screenshot"))
 
 -- Move focus with mainMod + arrow keys
 -- Move with mainMod + SHIFT
